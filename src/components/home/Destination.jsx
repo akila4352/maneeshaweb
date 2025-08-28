bbf94657a239570fcc08829a984ab09df57b196f
Akila Nirmal <akilanirmalzz4352@gmail.com>
2025-08-28 12:22:47 +0530

    v1.02

--- src/components/home/Destination.jsx
+++ src/components/home/Destination.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import CommonHeading from "../common/CommonHeading";
 // Example data
 const tours = [
